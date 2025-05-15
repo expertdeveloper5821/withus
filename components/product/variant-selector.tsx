554df387d85c9d706f6ca67d331924b48dd1a6d7
expertdeveloper5821 <expert.developer.5821@gmail.com>
2025-05-15 10:30:45 +0530

    changes

--- components/product/variant-selector.tsx
+++ components/product/variant-selector.tsx
@@ -41,7 +41,7 @@ export function VariantSelector({
     <form key={option.id}>
       <dl className="mb-8">
         <dt className="mb-4 text-[18px] md:text-[20px] tracking-wide text-black">{option.name}:<span className="font-normal ml-2 normal-case">{state[option.name.toLowerCase()]}</span></dt>
-        <dd className="flex flex-wrap gap-3">
+         <dd className="hidden md:flex flex-wrap gap-3" >
           {option.values.map((value) => {
             const optionNameLowerCase = option.name.toLowerCase();
 
@@ -103,7 +103,78 @@ export function VariantSelector({
               </button>
             );
           })}
-        </dd>
+        </dd> 
+        <dd
+  className={clsx(
+    'flex gap-3',
+    'overflow-x-auto flex-nowrap',
+    'sm:grid sm:grid-cols-4 sm:gap-4 sm:overflow-visible',  
+    'md:hidden lg:hidden',
+  )}
+>
+  {option.values.map((value) => {
+    const optionNameLowerCase = option.name.toLowerCase();
+    const optionParams = { ...state, [optionNameLowerCase]: value };
+
+    const filtered = Object.entries(optionParams).filter(([key, value]) =>
+      options.find(
+        (option) => option.name.toLowerCase() === key && option.values.includes(value)
+      )
+    );
+
+    const isAvailableForSale = combinations.find((combination) =>
+      filtered.every(
+        ([key, value]) => combination[key] === value && combination.availableForSale
+      )
+    );
+
+    const matchingCombination = combinations.find((combination) =>
+      filtered.every(
+        ([key, value]) => combination[key] === value && combination.availableForSale
+      )
+    );
+
+    const imageUrl = matchingCombination?.imageUrl;
+    const isActive = state[optionNameLowerCase] === value;
+
+    return (
+      <>
+      
+      <button
+        formAction={() => {
+          const newState = updateOption(optionNameLowerCase, value);
+          updateURL(newState);
+        }}
+        key={value}
+        aria-disabled={!isAvailableForSale}
+        disabled={!isAvailableForSale}
+        title={`${option.name} ${value}${!isAvailableForSale ? ' (Out of Stock)' : ''}`}
+        className={clsx(
+          'flex-none flex flex-col items-center justify-between rounded-lg border border-gray-300 bg-white text-black shadow-sm',
+          {
+            'ring-1 ring-red-600 border-red-600': isActive,
+            'ring-1 ring-transparent transition duration-300 ease-in-out hover:ring-red-600':
+              !isActive && isAvailableForSale,
+            'cursor-not-allowed bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300':
+              !isAvailableForSale,
+            'w-20 h-12': optionNameLowerCase === 'size',
+            'w-24 sm:w-auto sm:h-auto': optionNameLowerCase === 'color'
+          }
+        )}
+      >
+        {optionNameLowerCase === 'color' && imageUrl && (
+          <img
+            src={imageUrl}
+            alt={value}
+            className="w-full h-24 md:h-40 object-cover rounded-t-md"
+          />
+        )}
+        <span className="text-sm font-medium text-center p-4">{value}</span>
+      </button>
+      </>
+    );
+  })}
+</dd> 
       </dl>
     </form>
   ));
